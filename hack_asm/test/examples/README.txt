a87918b20cfb4a4bf537c641d866d0aeac8b31d0
Austin Bingham <austin.bingham@gmail.com>
2018-01-30 19:54:47 +0100

    Added approval tests based on official assembler.

---
+++ hack_asm/test/examples/README.txt
@@ -0,0 +1,3 @@
+A collection of ASM files and the assembled hack output.
+
+The hack output was generated using the official nand2tetris tools. We are using these files to test out assembler.
--- hack_asm/test/test_generation.py
+++ hack_asm/test/test_generation.py
@@ -1,13 +1,35 @@
+import pathlib
+
 from hypothesis import given
 import hypothesis.strategies as ST
+import pytest
 
+from hack_asm.generation import generate_microcode
 from hack_asm.parsing import parse_instruction
 
 
+EXAMPLES_DIR = pathlib.Path(__file__).parent / 'examples'
 MAX_15_BIT = 2 ** 15 - 1
+
+
+@pytest.fixture(params=EXAMPLES_DIR.glob('*.asm'))
+def canned_example(request):
+    hack_file = request.param.with_suffix('.hack')
+    return (request.param, hack_file)
 
 
 @given(ST.integers(min_value=0, max_value=MAX_15_BIT))
 def test_a_literal(val):
     instruction = parse_instruction('@{}'.format(val))
-    assert instruction.generate() == '0{:015b}'.format(val)
+    assert instruction.generate() == ['0{:015b}'.format(val)]
+
+
+def test_canned_example(canned_example):
+    asm_file, hack_file = canned_example
+
+    with open(asm_file, mode='rt') as stream:
+        microcode = generate_microcode(stream)
+        microcode = '\n'.join(microcode)
+
+    with open(hack_file, mode='rt') as stream:
+        assert microcode == stream.read().strip()
